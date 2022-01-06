1ffb9c83b38212243864f2a5c45d1aee575cb6d2
vishalanand125 <42240552+vishalanand125@users.noreply.github.com>
2022-01-06 19:44:52 +0530

    Add files via upload

---
+++ Untitled1.ps1
@@ -0,0 +1,13 @@
+﻿$FolderA = New-Item -Path "c:\" -Name "vish" -ItemType "directory"
+$FolderA.Update();
+$FolderA.BreakRoleInheritance($true);
+$FolderA.Update();
+Write-Output "Folder created " $FolderA.Url
+
+
+$FolderBURL=$list.ParentWebUrl +"/" + $FolderA.Url;
+write-host $FolderBURL;
+$FolderB = $list.AddItem($FolderBURL, [Microsoft.SharePoint.SPFileSystemObjectType]::Folder, "Folder B");
+$FolderB.Update();
+$FolderB.BreakRoleInheritance($false); 
+$FolderB.Update();
